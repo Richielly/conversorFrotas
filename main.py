044292f47fbbba12ff090c0a22bfe7ab3234e839
Richielly <richielly.positivo@gmail.com>
2023-04-20 13:53:57 -0300

    V_0.0.3

--- main.py
+++ main.py
@@ -2,12 +2,3 @@ from Validations.validationData import ValidationData as valid
 from Layout import bemLayout
 from Util import util, log
 
-b = bemLayout.BemLayout()
-v = valid()
-u = util.Util()
-
-l = log.Log()
-
-print(l.log("teste"))
-
-
