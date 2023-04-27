fe5c3cf1b7004eb582f36d9d0df625c017bc5f44
Richielly <richielly.positivo@gmail.com>
2023-04-27 16:54:37 -0300

    V_0.0.12

---
+++ venvconversorfrotas/Core/imports.py
@@ -0,0 +1,35 @@
+import validationData
+import core
+import util
+import log
+import messages
+import base
+import contentBase
+import typeConverter
+
+#Layuots
+import corLayout
+import modeloLayout
+import especieLayout
+import bemLayout
+import categoriaCnhLayout
+
+#LayoutsData
+
+#Util
+validation = validationData.ValidationData()
+core = core.Core()
+util = util.Util()
+log = log.Log()
+messages = messages.Messages()
+base = base.Base()
+contentBase = contentBase.contentBase
+typeConverter = typeConverter.TypeConverter()
+
+#Layout
+layoutCor = corLayout.CorLayout()
+layoutModelo = modeloLayout.ModeloLayout()
+layoutEspecie = especieLayout.EspecieLayout()
+layoutBem = bemLayout.BemLayout()
+layoutCategoriaCnh = categoriaCnhLayout.CategoriaCnhLayout()
+#layout = ()
